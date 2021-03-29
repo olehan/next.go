89a8b9c90074de818297635b47bc269aa5c24742
olehan <alihan.arshabekov@gmail.com>
2021-03-29 21:35:16 +0600

    refactor(add.cpp): remove args precheck

--- packages/add.node/add.cpp
+++ packages/add.node/add.cpp
@@ -15,21 +15,6 @@ namespace calc {
   void add(const FunctionCallbackInfo<Value>& args) {
     Isolate* isolate = args.GetIsolate();
 
-    // Check the number of arguments passed.
-    if (args.Length() < 2) {
-      // Throw an Error that is passed back to JavaScript
-      isolate->ThrowException(Exception::TypeError(
-          String::NewFromUtf8(isolate, "Wrong number of arguments")));
-      return;
-    }
-
-    // Check the argument types
-    if (!args[0]->IsNumber() || !args[1]->IsNumber()) {
-      isolate->ThrowException(Exception::TypeError(
-          String::NewFromUtf8(isolate, "Wrong arguments")));
-      return;
-    }
-
     // Perform the operation
     Local<Number> num = Number::New(isolate, Add(args[0].As<Number>()->Value(), args[1].As<Number>()->Value()));
 
